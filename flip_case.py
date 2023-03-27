9dd3ccb3ae586c846b1789fb24bfad75e1e1d5ff
Ryan Culp <105468921+giovannidonkey@users.noreply.github.com>
2023-03-26 20:52:04 -0400

    Add files via upload

---
+++ flip_case.py
@@ -0,0 +1,22 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    temp = temp.lower()
+    res = ""
+
+    for l in phrase:
+        if l.lower() == temp:
+            l = l.swapcase()
+        res += l
+
+    return res
---
+++ multiple_letter_count.py
@@ -0,0 +1,15 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    res = {}
+
+    for l in phrase:
+        res[l] = res.get(l, 0) + 1
+
+    return res
